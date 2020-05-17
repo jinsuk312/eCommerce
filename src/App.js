97ce95c090ea3ab8302eecba811d98ac74ccc39e
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-05-17 15:57:35 -0500

    form input and styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import ShopPage from './pages/shop/shop.component.jsx';
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -9,9 +12,12 @@ import './App.css';
 function App() {
 	return (
 		<div>
+			<Header />
+
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SignInAndSignUpPage} />
 			</Switch>
 		</div>
 	);
