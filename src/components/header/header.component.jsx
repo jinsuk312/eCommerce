8204bffa7c96eb2c1324ec4a31ba13b8617d314a
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-05-21 12:31:55 -0500

    using connect to get properties from reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 // tells CRA that you want a react component that renders an SVG, rather than its filename
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
@@ -30,4 +31,8 @@ const Header = ({ currentUser }) => (
 	</div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+	currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
