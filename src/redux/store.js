7ad549e24509b504dfc1c0093b25f95143b522e2
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-05-30 13:16:22 -0500

    redux-perist to persist our cart reducer across sessions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 // whenever actions get fired, we can catch them and display them
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
-//Add Here
+// Add Here
 const middlewares = [logger];
 // could use logger instead of ...middlewares
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// persisted version of our store
+const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
