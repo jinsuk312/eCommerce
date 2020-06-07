73c9a726f7687a05a17607bf2d82e2cf70bf578e
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-06-07 11:57:09 -0500

    collection routing and selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
+const CollectionPage = ({ collection }) => {
 	return (
-		<div className="category">
+		<div className="collection-page">
 			<h2>COLLECTION PAGE</h2>
 		</div>
 	);
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+	collections: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
